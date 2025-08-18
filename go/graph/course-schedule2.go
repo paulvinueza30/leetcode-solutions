d25ed6e910ab71e88c2009a1a247b7dd68de3d54
Paul Martin Vinueza <paulvinueza30@gmail.com>
2025-08-18 12:52:36 -0400

    course schedule 2 dfs the hands are getting faster :)

---
+++ go/graph/course-schedule2.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+func dfs2(curr int, v map[int]int, g map[int][]int, r *[]int) bool {
+	if v[curr] == 1 {
+		return false
+	}
+	if v[curr] == -1 {
+		return true
+	}
+	v[curr] = 1
+
+	for _, nei := range g[curr] {
+		if !dfs2(nei, v, g, r) {
+			return false
+		}
+	}
+	v[curr] = -1
+	*r = append(*r, curr)
+	return true
+}
+func findOrder(numCourses int, prereqs [][]int) []int {
+	var res []int
+	graph := make(map[int][]int)
+	visited := make(map[int]int)
+
+	for _, courses := range prereqs {
+		courseToTake := courses[0]
+		prereq := courses[1]
+		graph[courseToTake] = append(graph[courseToTake], prereq)
+	}
+
+	for i := range graph {
+		if !dfs2(i, visited, graph, &res) {
+			return nil
+		}
+
+	}
+
+	for i := range numCourses {
+		if visited[i] == 0 {
+			res = append(res, i)
+		}
+	}
+	return res
+}
+
+func main() {
+	fmt.Println(findOrder(3, [][]int{{1, 0}}))
+}
