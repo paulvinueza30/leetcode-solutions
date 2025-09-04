d50e39412f8c934d4f7682a416a6aa63c6612b74
Paul Martin Vinueza <paulvinueza30@gmail.com>
2025-09-04 00:32:49 -0400

    dp is my weakpoint :(

---
+++ go/1-D/coins.go
@@ -0,0 +1,18 @@
+package main
+
+func coinChange(coins []int, amount int) int {
+	dp := make([]int, amount + 1)
+	for i := range dp{
+		dp[i] = amount + 1
+	}
+	dp[0] = 0
+	for a := range (amount + 1){
+		for _, c := range coins{
+			dp[a] = 1 + dp[a - c]
+		}
+	}
+	if dp[amount] == amount + 1{
+		return -1
+	}
+	return dp[amount]
+}
