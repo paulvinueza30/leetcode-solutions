48e5fac2125500754964490515fe033f572b4aef
Paul Martin Vinueza <paulvinueza30@gmail.com>
2025-10-01 16:22:31 -0400

    patience sorting LIS

--- go/1-D/longest-inc-sub.go
+++ go/1-D/longest-inc-sub.go
@@ -21,20 +21,47 @@ package main
 // }
 //
 
-func lengthOfLIS(nums []int) int {
-	dp := make([]int, len(nums))
-	for i := range nums {
-		dp[i] = 1
-	}
-	res := 1
+// func lengthOfLIS(nums []int) int {
+// 	dp := make([]int, len(nums))
+// 	for i := range nums {
+// 		dp[i] = 1
+// 	}
+// 	res := 1
 
-	for i := len(nums) - 1 ; i >= 0 ; i--{
-		for j := i + 1 ; j < len(nums) ; j++{
-			if nums[i] < nums[j]{
-				dp[i] = max(dp[i], 1 + dp[j])
-				res = max(res, dp[i])
+// 	for i := len(nums) - 1 ; i >= 0 ; i--{
+// 		for j := i + 1 ; j < len(nums) ; j++{
+// 			if nums[i] < nums[j]{
+// 				dp[i] = max(dp[i], 1 + dp[j])
+// 				res = max(res, dp[i])
+// 			}
+// 		}
+// 	}
+// 	return res
+// }
+
+func lengthOfLIS(nums []int) int {
+	LIS := []int{}
+	
+	bs := func(target int) int{
+		l , r := 0 , len(LIS) - 1
+		for l <= r {
+			mid := (l + r) / 2
+			if LIS[mid] < target{
+				l = mid + 1
+			} else {
+				r = mid - 1
 			}
 		}
+		return l
 	}
-	return res
+	
+	for _, num := range nums {
+		if len(LIS) == 0 || LIS[len(LIS) - 1] < num {
+			LIS = append(LIS, num)
+		} else {
+			idx := bs(num)
+			LIS[idx] = num
+		}
+	}
+	return len(LIS)
 }
