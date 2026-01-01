cf41e7cd01a2a3a9780178d779c50fea1f3acdab
Paul Martin Vinueza <paulvinueza30@gmail.com>
2026-01-01 16:24:55 -0500

    valid

---
+++ go/rerun/stack/min-stack.go
@@ -0,0 +1,36 @@
+package stack
+
+type MinStack struct {
+	stack    []int
+	minStack []int
+}
+
+func Constructor() MinStack {
+	return MinStack{
+		stack:    make([]int, 0),
+		minStack: make([]int, 0),
+	}
+}
+
+func (this *MinStack) Push(val int) {
+	mostMin := val
+	if len(this.stack) != 0 {
+		mostMin = min(this.GetMin(), val)
+	}
+
+	this.stack = append(this.stack, val)
+	this.minStack = append(this.minStack, mostMin)
+}
+
+func (this *MinStack) Pop() {
+	this.stack = this.stack[:len(this.stack)-1]
+	this.minStack = this.minStack[:len(this.minStack)-1]
+}
+
+func (this *MinStack) Top() int {
+	return this.stack[len(this.stack)-1]
+}
+
+func (this *MinStack) GetMin() int {
+	return this.minStack[len(this.minStack)-1]
+}
