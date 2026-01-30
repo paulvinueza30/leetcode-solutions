46d278b11b9ff7f9d4b260cde3be2055fa8f2c58
paul vinueza <paulvinueza30@gmail.com>
2026-01-29 20:04:29 -0500

    first tryyyy

---
+++ go/rerun/graph/is-valid-bs.go
@@ -0,0 +1,29 @@
+package graph
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isValidBST(root *TreeNode) bool {
+	valid := func(treeMin, curr, treeMax int) bool {
+		if curr > treeMin && curr < treeMax {
+			return true
+		}
+		return false
+	}
+	var dfs func(r *TreeNode, treeMin, treeMax int)
+	flag := true
+	dfs = func(r *TreeNode, treeMin, treeMax int) {
+		if r == nil {
+			return
+		} else if !valid(treeMin, r.Val, treeMax) {
+			flag = false
+		}
+		dfs(r.Left, treeMin, r.Val)
+		dfs(r.Right, r.Val, treeMax)
+	}
+	dfs(root, -9999999999, 99999999999)
+	return flag
+}
