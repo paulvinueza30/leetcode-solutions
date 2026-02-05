119ea83bf12406876d96d323d6b8a7521d5d59c2
paul vinueza <paulvinueza30@gmail.com>
2026-02-05 13:53:13 -0500

    three sum no need for set

---
+++ go/rerun/two-pointers/3sum.go
@@ -0,0 +1,33 @@
+package twopointers
+
+import "slices"
+
+func threeSum(nums []int) [][]int {
+	res := make([][]int, 0)
+	slices.Sort(nums)
+	for i := 0; i < len(nums); i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		l, r := i+1, len(nums)-1
+		for l < r {
+			sum := nums[i] + nums[l] + nums[r]
+			if sum == 0 {
+				res = append(res, []int{nums[l], nums[r], nums[i]})
+				for l < r && nums[l] == nums[l+1] {
+					l++
+				}
+				for l < r && nums[r] == nums[r-1] {
+					r--
+				}
+				l++
+				r--
+			} else if sum < 0 {
+				l++
+			} else {
+				r--
+			}
+		}
+	}
+	return res
+}
